7911e877e2440f7c60a4853824ddbb607611476c
Satish Balay <balay@mcs.anl.gov>
2023-01-04 11:16:51 -0600

    fix:

--- config/examples/arch-ci-linux-cuda-single-cxx.py
+++ config/examples/arch-ci-linux-cuda-single-cxx.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
   import configure
   configure_options = [
     '--package-prefix-hash='+petsc_hash_pkgs,
-    '--with-make-test-np=10',
+    '--with-make-test-np=8',
     'COPTFLAGS=-g -O',
     'FOPTFLAGS=-g -O',
     'CXXOPTFLAGS=-g -O',
